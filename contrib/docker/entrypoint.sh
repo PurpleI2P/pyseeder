fba6eb4f3a80bbbc53367ecaab65222557308c8a
l-n-s <supervillain@riseup.net>
2017-07-30 05:10:49 -0400

    create default netDb dir

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -12,11 +12,14 @@ else
     PRIVKEY_FILE="$DATA_DIR/data/private_key.pem"
     CERT_FILE=` echo $DATA_DIR/data/$SIGNER_ID.crt | sed 's/@/_at_/' `
     RESEED_FILE="$DATA_DIR/output/i2pseeds.su3"
-    I2PD_DIR="/i2pd_data"
+    NETDB_DIR="/i2pd_data/netDb"
+    if [ ! -d $NETDB_DIR ]; then
+	    NETDB_DIR="/netDb"
+    fi
 
     $COMMAND keygen --signer-id $SIGNER_ID --no-encrypt \
         --private-key $PRIVKEY_FILE --cert $CERT_FILE
-    $COMMAND reseed --netdb $I2PD_DIR/netDb --signer-id $SIGNER_ID --no-encrypt \
+    $COMMAND reseed --netdb $NETDB_DIR --signer-id $SIGNER_ID --no-encrypt \
         --private-key $PRIVKEY_FILE --outfile $RESEED_FILE
     $COMMAND serve --private-key $PRIVKEY_FILE --cert $CERT_FILE --file $RESEED_FILE
 fi
